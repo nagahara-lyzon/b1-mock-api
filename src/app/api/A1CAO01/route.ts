f6401b136bf010416f066b4b45589a21b260e44d
nagaharashun <nagahara@lyzon.co.jp>
2024-12-19 18:27:37 +0900

    feat: add errorjson

--- src/app/api/A1CAO01/route.ts
+++ src/app/api/A1CAO01/route.ts
@@ -2,27 +2,26 @@ import { NextRequest, NextResponse } from 'next/server';
 import A1CAO01_202110 from '@/app/mocks/A1CAO01_202110.json';
 import A1CAO01_202111 from '@/app/mocks/A1CAO01_202111.json';
 import A1CAO01_202112 from '@/app/mocks/A1CAO01_202112.json';
+import A1CAO01_error from '@/app/mocks/A1CAO01_error.json';
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const simulateError = searchParams.get('error') === 'true';
+	const { searchParams } = new URL(request.url);
+	const simulateError = searchParams.get('error');
 	const month = searchParams.get('month');
 
-  const headers = {
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-    'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, PATCH, DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Credentials': 'true'
-  };
+	const headers = {
+		'Access-Control-Allow-Origin': 'http://localhost:3000',
+		'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, PATCH, DELETE',
+		'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		'Access-Control-Allow-Credentials': 'true',
+	};
 
-
-  if (simulateError) {
-    return NextResponse.json(
-      { message: 'Internal Server Error' },
-      { status: 500, headers }
-    );
-  }
-  let response = A1CAO01_202111;
+	if (simulateError === '1') {
+		return NextResponse.json({ message: 'Internal Server Error' }, { status: 500, headers });
+	} else if (simulateError === '2') {
+		return NextResponse.json(A1CAO01_error, { status: 500, headers });
+	}
+	let response = A1CAO01_202111;
 
 	if (month === '202110') {
 		response = A1CAO01_202110;
@@ -30,5 +29,5 @@ export async function GET(request: NextRequest) {
 		response = A1CAO01_202112;
 	}
 
-  return NextResponse.json(response, { headers });
+	return NextResponse.json(response, { headers });
 }
